df2846a977ee86d8efe83bb525709f51a7d7536f
Dominik Mueller <dominik.mueller.18@googlemail.com>
2015-11-17 08:45:48 +0100

    Added uml_diagram and dependencies

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,2 +1,4 @@
 class Calendar < ActiveRecord::Base
+  has_many :events, depent: :destroy
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_one :calendar, depent :delete
+  has_many :courses
+  has_one :team
+  belongs_to :team
 end
