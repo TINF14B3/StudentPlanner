045cc1986b97794a47d90f88c3703fb1d3ef6c0a
Dominik Mueller <dominik.mueller.18@googlemail.com>
2015-12-02 10:51:23 +0100

    Added styling and SignUp routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   has_many :courses
   has_one :team
   belongs_to :team
+
+  validates :firstname, presence: true, length: {maximum: 50}
+  validates :lastname, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :mail, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 end
