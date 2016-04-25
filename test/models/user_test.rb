2de191c6a041a931b2079735e015d98dcadeda41
Dominik Mueller <dominik.mueller.18@googlemail.com>
2016-04-25 22:18:38 +0200

    Working tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(firstname: "Example", lastname: "User", mail: "example@mail.org", password: "123456")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.firstname = "      "
+    assert_not @user.valid?
+  end
+   #test "the truth" do
+   #  assert true
+   #end
 end
